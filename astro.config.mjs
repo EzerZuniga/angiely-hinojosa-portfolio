43c3fc0d02884081033244b542579a694af03723
Ezer Benito Zúñiga chura <76848731@continental.edu.pe>
2026-01-17 21:50:11 -0500

    confif push

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap()],
   site: "https://angiely-hinojosa.vercel.app",
   trailingSlash: "ignore",
   build: {
